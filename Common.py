03a7ca334fe90563bacae5bc757175401b7f9004
makandat <tadnakam@outlook.jp>
2019-05-09 16:14:53 +0900

    詳細エラー情報取得関数(errorInfo)追加

--- Common.py
+++ Common.py
@@ -1,6 +1,8 @@
+# Common.py
 # -*- code=utf-8 -*-
-#   ver 2.21  2019-02-22
+#   ver 2.30  2019-05-09
 import sys
+import linecache
 import os
 import subprocess
 import logging
@@ -99,6 +101,16 @@ def log(msg:str) -> None:
 # ログエラー出力
 def error(msg:str) -> None:
   logger.error(str(msg))
+
+# 例外発生時の詳細情報
+def errorInfo() :
+  exc_type, exc_obj, tb = sys.exc_info()
+  f = tb.tb_frame
+  lineno = tb.tb_lineno
+  filename = f.f_code.co_filename
+  linecache.checkcache(filename)
+  line = linecache.getline(filename, lineno, f.f_globals)
+  return (filename, lineno, line.strip(), exc_obj)
 
 # 変数が有効かどうか
 def isset(v:Any) -> bool:
