7c0e9934cf256cfab108dab56146b706f57377e7
makandat <tadnakam@outlook.jp>
2019-07-25 08:20:49 +0900

    JQuery bag

--- JQuery.py
+++ JQuery.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# JQuery クラス  Version 1.0  2019-05-12
-
-VERSION   = "1.0"
+# JQuery クラス  Version 1.01  2019-07-24
+import FileSystem as fs
+VERSION   = "1.01"
 
 
 # クラス
@@ -10,12 +10,8 @@ class JQuery :
   # コンストラクタ
   def __init__(self, cdnver="3.4.1") :
     self.lines = []
-    if cdnver == "" or cdnver == None :
-      pass
-    else :
-      self.lines.append("<script src=\"https://code.jquery.com/jquery-{0}.min.js\"></script>".format(cdnver))
-    self.lines.append("<script>")
-    self.lines.append(" $(function() {")
+    self.literals = []
+    self.cdnver = cdnver
     return
 
   # イベントハンドラのグループを終わりにする。
@@ -24,23 +20,32 @@ class JQuery :
     return
 
   # 文字列に変換する。
-  def tostring() :
+  def tostring(self) :
     s = ""
-    s += "\n".join(self.lines)
-    s += "</script>\n"
+    if not (self.cdnver == "" or self.cdnver == None) :
+      s += "<script src=\"https://code.jquery.com/jquery-{0}.min.js\"></script>".format(self.cdnver)
+    s += "\n<script>\n"
+    if len(self.lines) > 0 :
+      s += " $(function() {\n"
+      s += "\n".join(self.lines)
+      s += "});\n";
+    if len(self.literals) > 0 :
+      s += "\n".join(self.literals)
+    s += "\n</script>\n"
     return s
 
   # ファイル保存する。
   def save(self, filePath) :
+    fs.writeAllText(filePath, self.tostring())
     return
 
   # 文字列をそのままコードのリストに追加する。
   def literal(self, code) :
-    self.lines.append(code)
+    self.literals.append(code)
     return
 
   #  コントロールイベント
-  def control(self, selector_name, selector_type="id", event="click") :
+  def event_handler(self, selector_name, selector_type="id", event="click", code="\n") :
     prefix = ""
     if selector_type == 'id' :
       prefix = "#"
@@ -48,31 +53,36 @@ class JQuery :
       prefix = '.'
     else :
       pass
-    s = "$(\"{0}{1}\").".format(prefix, selector_name)
-    s += "{0}(function(){".format(event)
-    s += "  // todo: ここにイベントハンドラのコードを追加する。"
-    s += "});"
-    self.lines(s)
+    s = "  $(\"{0}{1}\").".format(prefix, selector_name)
+    s += "{0}(function()".format(event) + "{\n"
+    s += code
+    s += "  });\n"
+    self.lines.append(s)
     return s
 
   #  Ajax で URL からデータを取得
-  def ajax_get(self, url, parameters, id=None) :
-    s = "$.get({0}, {1}, function(data) {".format(url, parameters)
-    if id == None :
-      s += "  // todo: ここにデータを受け取ったときのコードを追加する。"
+  def ajax_get(self, url, parameters, json=False, id=None, code="") :
+    if json :
+      s = "  $.getJSON(\"{0}\", {1}, function(data) ".format(url, parameters) + "{\n"
     else :
-      s += "  $("#{0}").text(data);".format(id);
-    s += "});"
-    return
+      s = "  $.get(\"{0}\", {1}, function(data) ".format(url, parameters) + "{\n"
+    if id == None or id == "":
+      s += code
+    else :
+      s += " 　 $(\"#" + id + "\").text(data);"
+    s += "\n  });\n"
+    self.lines.append(s)
+    return s
 
   #  Ajax で URL へフォームデータをポスト
-  def ajax_post(self, url, parameters, id) :
-    s = "$.post({0}, {1}, function(data) {".format(url, parameters)
-    if id == None :
-      s += "  // todo: ここにデータを受け取ったときのコードを追加する。"
+  def ajax_post(self, url, parameters, id, code="") :
+    s = "  $.post(\"{0}\", {1}, function(data) ".format(url, parameters) + "{\n"
+    if id == None or id == "":
+      s += code
     else :
-      s += "  $("#{0}").text(data);".format(id);
-    s += "});"
-    return
+      s += "   $(\"#{0}\").text(data);".format(id);
+    s += "\n   });\n"
+    self.lines.append(s)
+    return s
 
     
