ef9b3f170c04ee9335513a80822d802f8505569b
makandat <tadnakam@outlook.jp>
2023-03-21 20:40:40 +0900

    MySQL.py のコネクタを MySQLDb に変更した。

--- MySQL.py
+++ MySQL.py
@@ -1,134 +1,62 @@
-# -*- coding: utf-8 -*-
+# -*- code=utf-8 -*-
 # Version 0.50  2018-09-12
 # Version 1.00  2018-10-03
 # Version 1.10  2018-12-01
 # Version 1.20  2019-04-09
-# Version 1.30  2019-07-20
-# Version 2.00  2021-09-11
-# Version 2.10  2021-09-15
-# Version 2.20  2023-03-12
-#    To install mysql connector
-#  sudo pip3 install mysql-connector-python
-import json, os
-import mysql.connector
-from Py365Lib import Common
+# version 2.0.0 2023-03-21
+#    To install mysqlclient
+#  sudo pip3 install mysqlclient
+import MySQLdb
+import json
 
 class MySQL :
-    APPCONF = "AppConf.ini"
+  CONF = "mysqlclient.json"
 
-    # コンストラクタ
-    def __init__(self, uid="", pwd="", db="", host="localhost") :
-        self.__config = {"user":uid, "password":pwd, "host":host, "database":db}
-        self.__rows = {}
-        # パラメータが無効の場合は、AppConf.ini から取得する。
-        if uid == "" :
-            # APPCONF を使う。
-            self.readAppConf()
-        elif pwd == "" and db == "" :
-            # uid をコンフィグファイルとして使う
-            self.readAppConf(uid)
-        else :
-            self.__config = {"user":uid, "password":pwd, "database":db, "host":host}
-        # 接続する。
-        self.__client = mysql.connector.connect(**self.__config)
-        # カーソルを取得する。
-        self.__cursor = self.__client.cursor()
+  # コンストラクタ
+  def __init__(self, user="", password="", database="", host="localhost") :
+    # パラメータが無効の場合は、CONF から取得する。
+    if user == "" :
+      with open(MySQL.CONF, "r") as f:
+        conf = json.load(f)
+        self._client = MySQLdb.connect(user=conf["user"], password=conf["password"], host=conf["host"], database=conf["user"])
+    else :
+      # 接続する。
+      self._client = MySQLdb.connect(host, user, password, database)
+    # カーソルを取得する。
+    self._cursor = self._client.cursor()
+    return
 
-    # AppConf.ini を読んで接続情報を得る。
-    def readAppConf(self, conf=None) :
-        uid = ""
-        pwd = ""
-        db = ""
-        host = "localhost"
-        if conf == None :
-            conf = MySQL.APPCONF
-        #Common.log(conf)
-        root, ext = os.path.splitext(conf)
-        if ext == '.ini' :
-            with open(conf) as f :
-                for line in f :
-                    pair = line.strip().split('=')
-                    if (pair[0] == 'uid') :
-                        uid = pair[1]
-                    elif (pair[0] == 'pwd') :
-                        pwd = pair[1]
-                    elif pair[0] == 'db' :
-                        db = pair[1]
-                    elif pair[0] == 'host' :
-                        host = pair[1]
-                    else :
-                        pass
-            self.__config = {"user":uid, "password":pwd, "host":host, "database":db}
-        elif ext == ".json" :
-            with open(conf) as f :
-                str = f.read()
-                #Common.log("MySQL.readAppConf : " + str)
-                self.__config = json.loads(str)
-        else :
-            raise "No MySQL config file."
+  # クエリーを行う。
+  def query(self, sql) :
+    self._cursor.execute(sql)
+    rows = self._cursor.fetchall()
+    return rows
 
-    # カーソル
-    def cursor(self, sql) :
-        self.__cursor.execute(sql)
-        return self.__cursor
+  # コマンドを実行する。
+  def execute(self, sql) :
+    n = 0
+    try :
+      n = self._cursor.execute(sql)
+      self._client.commit()
+    except :
+      self._client.rollback()
+      raise
+    return n
 
-    # クエリーを行う。
-    def query(self, sql) :
-        self.__cursor.execute(sql)
-        self.__rows = self.__cursor.fetchall()
-        return self.__rows
+  # 値を1つだけ返すクエリーを実行し、その値を得る。
+  def getValue(self, sql) :
+    result = None
+    self._cursor.execute(sql)
+    row = self._cursor.fetchone()
+    return row[0]
 
-    # 結果が1行のクエリーを行う。
-    def getRow(self, sql) :
-        self.__cursor.execute(sql)
-        self.__rows = self.__cursor.fetchall()
-        if self.__rows :
-          return self.__rows[0]
-        else :
-          return None
+  # 行を1つだけ返すクエリーを実行し、その値を得る。
+  def getRow(self, sql) :
+    result = None
+    self._cursor.execute(sql)
+    rows = self._cursor.fetchone()
+    return rows
 
-    # クエリー結果行数を取得する。
-    @property
-    def rows(self) :
-        return len(self.__rows)
-
-    # コマンドを実行する。
-    def execute(self, sql) :
-        n = 0
-        try :
-            n = self.__cursor.execute(sql)
-            self.__client.commit()
-        except :
-            self.__client.rollback()
-            raise
-        return n
-
-    # 値を1つだけ返すクエリーを実行し、その値を得る。
-    def getValue(self, sql) :
-        result = None
-        self.__cursor.execute(sql)
-        self.__rows = self.__cursor.fetchall()
-        if len(self.__rows) >= 1 :
-            row = self.__rows[0]
-            result = row[0]
-        return result
-
-    # カーソルを閉じる。
-    def cursorClose(self) :
-        self.__cursor.close()
-
-    # カラム名を得る。
-    def getFieldNames(self, table="") :
-      if table != "" :
-        self.cursor("SELECT * FROM " + table + " LIMIT 1")
-      num_fields = len(self.__cursor.description)
-      field_names = [i[0] for i in self.__cursor.description]
-      return field_names
-
-    # 接続情報
-    @property
-    def connectInfo(self) :
-        return (self.__config)
-
-
-
+  # カーソルを閉じる。
+  def cursorClose(self) :
+    self._cursor.close()
--- test/testMySQL.py
+++ test/testMySQL.py
@@ -14,26 +14,17 @@ else :
 client = mysql.MySQL()
 
 if testNo == 1 :
-    print(client.connectInfo)
-elif testNo == 2 :
-    rows = client.query("SELECT * FROM m_tables")
-    if client.rows > 0 :
-        for row in rows :
-            print(row)
+  rows = client.query("SELECT * FROM m_tables")
+  for row in rows :
+    print(row)
+elif testNo == 2:
+  n = client.execute("INSERT INTO YJFX_Asset(`date`,`asset`,`profit_loss`) VALUES('2011-09-16',4574314,-874320)")
+  print(n)
 elif testNo == 3:
-    n = client.execute("INSERT INTO YJFX_Asset(`date`,`asset`,`profit_loss`) VALUES('2011-09-16',4574314,-874320)")
-    print(n)
-elif testNo == 4:
-    n = client.getValue("SELECT count(*) FROM YJFX_Asset")
-    print(n)
-elif testNo == 5 :
-    cur = client.cursor("SELECT `database`, name FROM m_tables")
-    while True :
-        row = cur.fetchone()
-        if Common.isset(row) :
-            print(row[0] + "." + row[1])
-        else :
-            break
-    client.cursorClose()
+  n = client.getValue("SELECT count(*) FROM YJFX_Asset")
+  print(n)
+elif testNo == 4 :
+  row = client.getRow("SELECT `database`, name FROM m_tables")
+  print(row[0], row[1])
 else :
     print("不正なテスト番号です。")
